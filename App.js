dcac2b23f9e01ab36fcb4bd35e2a3b6e97b2202d
As-Shovo <akbarshovo18@gmail.com>
2021-12-19 01:15:32 +0600

    input & piker

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { KeyboardAvoidingView, Platform, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [value, setValue] = useState('');
+
+  // const handleValue = () => {
+  //   setValue('hello Java')
+  // }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -12,12 +21,18 @@ export default function App() {
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={styles.writeTaskWrapper}
       >
-        <TextInput style={styles.input} placeholder={'write a task'}></TextInput>
+        <TextInput style={styles.input} placeholder={' Number'}></TextInput>
+        <Picker style={styles.piker}
 
-
+          onValueChange={(itemValue, itemIndex) =>
+            setValue(itemValue)
+          }>
+          <Picker.Item label="Kilometer" value="kilometer" />
+          <Picker.Item label="Meter" value="meter" />
+          <Picker.Item label="Sentimeter" value="sentimer" />
+        </Picker>
         <TouchableOpacity >
-          <View >
-          </View>
+          <View>{value}</View>
         </TouchableOpacity>
       </KeyboardAvoidingView>
     </SafeAreaView>
@@ -42,6 +57,42 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     color: "#fff"
   },
+
+  writeTaskWrapper: {
+    width: '100%',
+    flexDirection: 'colomn',
+    alignItems: "center",
+    marginLeft: 5,
+    marginRight: 5,
+
+  },
+
+  input: {
+    paddingVertical: 15,
+    display: "inline-block",
+    paddingHorizontal: 15,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: "#fff",
+    borderRadius: 50,
+    borderColor: '#C0C0C0',
+    width: 250
+  },
+
+  piker: {
+    width: 250,
+    borderRadius: 50,
+    backgroundColor: "#fff",
+    height: 40,
+    paddingHorizontal: 15,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: "#fff",
+    borderRadius: 50,
+    borderColor: '#C0C0C0',
+
+
+  }
 
 
 
